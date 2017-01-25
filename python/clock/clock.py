222384e7def22b9a92d4b32c560aae0b00021cdf
crashtack <crashtack@gmail.com>
2017-01-25 11:31:20 -0800

    Clock passing all tests

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,12 +1,12 @@
 class Clock(object):
-    """ Returns time from inputed hours and minutes """
+    """ A Clock class that ignores date """
 
     def __init__(self, hours=None, minutes=None):
         """ Initialize the Clock object """
         if hours is None:
             self.hours = 0
         else:
-            self.hours = hours
+            self.hours = hours % 24
 
         if minutes is None:
             self.minutes = 0
@@ -17,23 +17,29 @@ class Clock(object):
         self.time_min = self.time_min % 1440
 
     def add(self, minute=None):
+        """ Add minutes to the time and check for overflow """
         self.time_min += minute
         self.time_min = self.time_min % 1440
 
+        return '{:02d}:{:02d}'.format(
+            int(self.time_min / 60) % 24,
+            self.time_min % 60
+        )
+
     def __eq__(self, other):
+        """ Check if time_min is equal """
         if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
+            return self.time_min == other.time_min
         else:
             return False
 
     def __ne__(self, other):
+        """ Check if time_min is not equal """
         return not self.__eq__(other)
 
     def __str__(self):
         """ Returns the hh:mm time format """
         return '{:02d}:{:02d}'.format(
-            self.hours,
-            self.minutes
-            # int(self.time_min / 24),
-            # self.time_min % 60
+            int(self.time_min / 60) % 24,
+            self.time_min % 60
         )
